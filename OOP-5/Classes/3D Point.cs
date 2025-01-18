2f8ff22a46484a10ca8854780420543c5633f456
Abdelrahmany20 <abdelrahman.yasser.m17@gmail.com>
2025-01-18 11:43:44 +0200

    First Project

---
+++ OOP-5/Classes/3D Point.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_5.Classes
+{
+    public class _3D_Point : IComparable ,ICloneable
+    {
+
+        public double point1 { get; set; }
+        public double point2 { get; set; }
+        public double point3 { get; set; }
+
+
+
+
+        public _3D_Point (double x=0, double y=0, double z=0)
+        {
+            point1 = x;
+            point2 = y;
+            point3 = z;
+
+
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({point1}, {point2}, {point3})";
+        }
+
+        public int CompareTo(object? obj)
+        {
+
+            _3D_Point passedpoint = (_3D_Point)obj;
+            if(this.point1 > passedpoint.point1)
+                return 1;
+            else if(this.point2 > passedpoint.point2)
+                return -1;
+            else return 0;
+            
+        }
+
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+} 
--- OOP-5/Program.cs
+++ OOP-5/Program.cs
@@ -1,10 +1,201 @@
-﻿namespace OOP_5
+﻿using OOP_5.Classes;
+using System.Security.Cryptography.X509Certificates;
+
+namespace OOP_5
 {
-    internal class Program
+    internal class Program 
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region First Project
+
+            #region Q01
+
+            //_3D_Point Test = new _3D_Point(3,5,7);
+            //Console.WriteLine(Test.point1);
+
+
+            //_3D_Point Test2 = new _3D_Point();
+            //Console.WriteLine(Test2.point2);
+
+            #endregion
+
+
+            #region Q02
+
+            //_3D_Point Test3 = new _3D_Point(10, 10, 10);
+
+            //Console.WriteLine(Test3.ToString());
+
+            #endregion
+
+
+            #region Q03
+            //-----------------------TryParse -------------------------//
+
+
+            //double checkCoordinate(string coordinate)
+            //{
+            //double value;
+            //bool isValid;
+
+            //do
+            //{
+
+            //Console.Write($"Enter {coordinate}: ");
+            //string input = Console.ReadLine();
+
+            //isValid = double.TryParse(input, out value);
+
+            //if (!isValid)
+            //{
+            //Console.WriteLine("Invalid input !");
+            //}
+            //} while (!isValid);
+
+            //return value;
+            //}
+
+
+            //-----------------------convert-------------------------//
+
+
+            //static double checkCoordinate(string coordinateName)
+            //{
+            //while (true)
+            //{
+            //try
+            //{
+            //Console.Write($"Enter {coordinateName}: ");
+            //return Convert.ToDouble(Console.ReadLine());
+            //}
+            //catch
+            //{
+            //Console.WriteLine("Invalid input");
+            //}
+            //}
+
+            //}
+
+
+            //Console.WriteLine("Enter coordinates for Point P1:");
+            //double x1 = checkCoordinate("X1");
+            //double y1 = checkCoordinate("Y1");
+            //double z1 = checkCoordinate("Z1");
+
+            //Console.WriteLine("\nEnter coordinates for Point P2:");
+            //double x2 = checkCoordinate("X2");
+            //double y2 = checkCoordinate("Y2");
+            //double z2 = checkCoordinate("Z2");
+
+            //Console.WriteLine("\nPoints you entered:");
+            //Console.WriteLine($"P1: ({x1}, {y1}, {z1})");
+            //Console.WriteLine($"P2: ({x2}, {y2}, {z2})");
+
+
+
+            #endregion
+
+
+            #region Q04
+
+            //_3D_Point P1 = new _3D_Point(1, 2, 3);
+            //_3D_Point P2 = new _3D_Point(1, 2, 3);
+
+
+            //if (P1 == P2)
+            //Console.WriteLine("P1 and P2 are equal.");
+            //else
+            //Console.WriteLine("P1 and P2 are not equal.");
+
+
+            // it doesn't work correctly because == doesn't compare the values put compare the references
+
+
+
+            #endregion
+
+            #region Q05
+            //_3D_Point[] points =
+            //{
+
+            //new _3D_Point (4,5,6) ,
+            //new _3D_Point (1,2,3),
+            //new _3D_Point (7,8,9)
+
+
+            //};
+
+            //int result = points[1] .CompareTo (points[2]);
+            //Console.WriteLine(result);
+
+
+
+            //------------------------------------------------//
+
+
+
+            //Array.Sort(points);
+
+            //foreach (_3D_Point point in points)
+            //{
+            //Console.WriteLine(point);
+
+            //}
+
+
+
+
+
+            //------------------------------------------------//
+
+
+
+            //for (int i = 0; i < points?.Length; i++)
+
+
+            //{
+            //for (int j = 0; j < points?.Length - i - 1; j++)
+
+            //{
+            //if (points[j].CompareTo(points[j + 1])==1)
+            //{
+            //Swap(points[j], points[j + 1])
+
+
+            //}
+            //}
+
+            //}
+
+
+            //public int CompareTo(Program? other)
+            //{
+            //throw new NotImplementedException();
+            //}
+
+            #endregion
+
+
+            #region Q06
+
+            //public class _3D_Point : IComparable, ICloneable
+            //{
+
+
+                   //public object Clone()
+                  //{
+                 //throw new NotImplementedException();
+                  //}
+            //}
+
+            #endregion
+
+            #endregion
         }
+
+
     }
 }
+
